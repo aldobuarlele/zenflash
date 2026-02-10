73f9a7a1fca09ab7fac3577253156822222819ee
Aldo Buarlele <aldobuarlele@Aldos-MacBook-Air.local>
2026-02-10 22:10:56 +0700

    feat: implement real Japanese NLP analysis using Kuromoji library

--- backend-core/src/main/java/com/zenflash/grpc/ZenFlashServer.java
+++ backend-core/src/main/java/com/zenflash/grpc/ZenFlashServer.java
@@ -1,32 +1,41 @@
 package com.zenflash.grpc;
 
+import com.atilika.kuromoji.ipadic.Token;
+import com.atilika.kuromoji.ipadic.Tokenizer;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 @Slf4j
 @GrpcService
 public class ZenFlashServer extends NlpServiceGrpc.NlpServiceImplBase {
 
+    private final Tokenizer tokenizer = new Tokenizer();
+
     @Override
     public void analyzeSentence(AnalyzeRequest request, StreamObserver<AnalyzeResponse> responseObserver) {
-        log.info("Received gRPC request with text: {}", request.getText());
+        log.info("Analyzing Japanese text: {}", request.getText());
 
-        Token testToken = Token.newBuilder()
-                .setSurface(request.getText())
-                .setReading("test_reading")
-                .setBaseForm("test_base")
-                .setPartOfSpeech("Noun")
-                .build();
+        List<Token> tokens = tokenizer.tokenize(request.getText());
+        AnalyzeResponse.Builder responseBuilder = AnalyzeResponse.newBuilder();
 
-        AnalyzeResponse response = AnalyzeResponse.newBuilder()
-                .addTokens(testToken)
-                .setSentiment("Testing_Active")
-                .build();
+        for (Token token : tokens) {
+            com.zenflash.grpc.Token grpcToken = com.zenflash.grpc.Token.newBuilder()
+                    .setSurface(token.getSurface())
+                    .setReading(token.getReading() != null ? token.getReading() : "")
+                    .setBaseForm(token.getBaseForm() != null ? token.getBaseForm() : "")
+                    .setPartOfSpeech(token.getAllFeaturesArray()[0])
+                    .build();
 
-        responseObserver.onNext(response);
+            responseBuilder.addTokens(grpcToken);
+        }
+
+        responseBuilder.setSentiment("Analyzed");
+
+        responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
 
-        log.info("Successfully sent gRPC response");
+        log.info("Analysis completed with {} tokens found", tokens.size());
     }
 }
